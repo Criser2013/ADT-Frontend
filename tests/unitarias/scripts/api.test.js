49e0f6085bedf838d7ed7fff0f8f3621633db48b
Cristian O. <84862634+Criser2013@users.noreply.github.com>
2025-07-29 14:26:36 +0000

    Se añadieron pruebas unitarias a las funciones para ver la información de los usuarios en el API.

--- tests/unitarias/scripts/api.test.js
+++ tests/unitarias/scripts/api.test.js
@@ -1,7 +1,7 @@
 import { jest, expect, describe, test } from '@jest/globals';
-import { generarDiagnostico } from "../../../src/services/Api";
+import { generarDiagnostico, verUsuario, verUsuarios } from "../../../src/services/Api";
 
-describe("Validar la funcion 'descargarArchivo'", () => {
+describe("Validar la funcion 'generarDiagnostico'", () => {
     test("CP - 70", async () => {
         global.fetch = jest.fn();
         global.fetch.mockImplementation(() =>
@@ -65,6 +65,147 @@ describe("Validar la funcion 'descargarArchivo'", () => {
                 "Authorization": "Bearer token invalido"
             },
             body: JSON.stringify({ vih: 0, edad: 1, trombofilia: 2 })
+        });
+    });
+});
+
+describe("Validar la funcion 'verUsuarios'", () => {
+    test("CP - 81", async () => {
+        const resEsperada = [
+            { nombre: "persona1", estado: true, rol: 0, "ultima_conexion": "13/07/2025 05:25 PM", correo: "correo@correo.com" },
+            { nombre: "persona2", estado: false, rol: 1001, "ultima_conexion": "14/07/2025 05:25 PM", correo: "correo1@correo.com" }
+        ];
+        global.fetch = jest.fn();
+        global.fetch.mockImplementation(() =>
+            Promise.resolve({
+                ok: true, status: 200,
+                json: () => Promise.resolve({ usuarios: resEsperada })
+            })
+        );
+        const res = await verUsuarios("token");
+        expect(res).toEqual({ success: true, data: resEsperada, error: null });
+
+        expect(global.fetch).toHaveBeenCalledTimes(1);
+        expect(global.fetch).toHaveBeenCalledWith(
+            "http://localhost:5000/admin/usuarios", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer token"
+            },
+        });
+    });
+
+    test("CP - 82", async () => {
+        global.fetch = jest.fn();
+        global.fetch.mockImplementation(() =>
+            Promise.resolve({
+                ok: false, status: 403,
+                json: () => Promise.resolve({ error: "Token inválido" })
+            })
+        );
+        const res = await verUsuarios("token invalido");
+        expect(res).toEqual({ success: false, error: "Token inválido", data: null });
+
+        expect(global.fetch).toHaveBeenCalledTimes(1);
+        expect(global.fetch).toHaveBeenCalledWith(
+            "http://localhost:5000/admin/usuarios", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer token invalido"
+            }
+        });
+    });
+
+    test("CP - 83", async () => {
+        global.fetch = jest.fn();
+        global.fetch.mockImplementation(() => {
+            throw new Error("Error ocurrido.")
+        });
+        const res = await verUsuarios("token invalido");
+        expect(res).toEqual({ success: false, data: null, error: "Ha ocurrido un error al cargar los usuarios. Por favor reintenta nuevamente." });
+
+        expect(global.fetch).toHaveBeenCalledTimes(1);
+        expect(global.fetch).toHaveBeenCalledWith(
+            "http://localhost:5000/admin/usuarios", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer token invalido"
+            }
+        });
+    });
+});
+
+
+describe("Validar la funcion 'verUsuario'", () => {
+    test("CP - 84", async () => {
+        const resEsperada = {
+            correo: "correo1@gmail.com",
+            nombre: "persona1", rol: 0, estado: true,
+            ultima_conexion: "23/06/2025 11:19 AM"
+        };
+
+        global.fetch = jest.fn();
+        global.fetch.mockImplementation(() =>
+            Promise.resolve({
+                ok: true, status: 200,
+                json: () => Promise.resolve(resEsperada)
+            })
+        );
+        const res = await verUsuario("token", "correo1@gmail.com");
+        expect(res).toEqual({ success: true, data: resEsperada, error: null });
+
+        expect(global.fetch).toHaveBeenCalledTimes(1);
+        expect(global.fetch).toHaveBeenCalledWith(
+            "http://localhost:5000/admin/usuarios/correo1%40gmail%2Ecom", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer token"
+            },
+        });
+    });
+
+    test("CP - 85", async () => {
+        global.fetch = jest.fn();
+        global.fetch.mockImplementation(() =>
+            Promise.resolve({
+                ok: false, status: 404,
+                json: () => Promise.resolve({error: "El usuario no existe"})
+            })
+        );
+        const res = await verUsuario("token valido", "correo1@gmail.com");
+        expect(res).toEqual({ success: false, error: "El usuario no existe", data: null });
+
+        expect(global.fetch).toHaveBeenCalledTimes(1);
+        expect(global.fetch).toHaveBeenCalledWith(
+            "http://localhost:5000/admin/usuarios/correo1%40gmail%2Ecom", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer token valido"
+            }
+        });
+    });
+
+    test("CP - 86", async () => {
+        global.fetch = jest.fn();
+        global.fetch.mockImplementation(() => {
+            throw new Error("Error ocurrido.")
+        });
+        const res = await verUsuario("token invalido", "correo1@gmail.com");
+        expect(res).toEqual({ success: false, data: null, error: "Ha ocurrido un error al cargar los datos del usuario. Por favor reintenta nuevamente." });
+
+        expect(global.fetch).toHaveBeenCalledTimes(1);
+        expect(global.fetch).toHaveBeenCalledWith(
+            "http://localhost:5000/admin/usuarios/correo1%40gmail%2Ecom", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer token invalido"
+            }
         });
     });
 });
