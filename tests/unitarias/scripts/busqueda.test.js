ec611bfe819f8422ef5635439d02c3e644c61af0
Cristian O. <84862634+Criser2013@users.noreply.github.com>
2026-05-27 11:04:12 -0500

    Documentación del módulo Busqueda mejorada. Casos de prueba correspondientes actualizados.

--- tests/unitarias/scripts/busqueda.test.js
+++ tests/unitarias/scripts/busqueda.test.js
@@ -1,26 +1,39 @@
 import { expect, describe, test } from '@jest/globals';
 import { buscar } from '../../../src/utils/Busqueda';
 describe("Validar la función 'buscar'", () => {
-    test("CP - 60", () => {
-        const res = buscar([
+    // ---------------- Parámetros de prueba ----------------
+    const params1 = {
+        datos: [
             { nombre: "jose", edad: 20, cedula: "193456" },
             { nombre: "maria", edad: 35, cedula: "654321" },
             { nombre: "mario", edad: 99, cedula: "654311" }
-        ], "2", ["edad", "cedula"]
-        )
-        expect(res).toEqual([
+        ],
+        termino: "2",
+        campos: ["edad", "cedula"]
+    };
+    const params2 = {
+        datos: [
+            { nombre: "jose", edad: 20, cedula: "193456" },
+            { nombre: "maria", edad: 35, cedula: "654321" },
+            { nombre: "mario", edad: 99, cedula: "654311" }
+        ],
+        termino: "mario",
+        campos: ["edad", "cedula"]
+    }
+    
+    // ---------------- Respuestas esperadas ----------------
+    const res1 = [
             { nombre: "jose", edad: 20, cedula: "193456" },
             { nombre: "maria", edad: 35, cedula: "654321" }
-        ]);
-    });
+        ];
+    const res2 = [];
 
-    test("CP - 61", () => {
-        const res = buscar([
-            { nombre: "jose", edad: 20, cedula: "193456" },
-            { nombre: "maria", edad: 35, cedula: "654321" },
-            { nombre: "mario", edad: 99, cedula: "654311" }
-        ], "mario", ["edad", "cedula"]
-        )
-        expect(res).toEqual([]);
+    test.each([
+        ["60", params1, res1]
+    ])("CP - %s", (idPrueba, params, resEsperado) => {
+        const { datos, termino, campos } = params;
+
+        const res = buscar(datos, termino, campos);
+        expect(res).toEqual(resEsperado);
     });
 });
