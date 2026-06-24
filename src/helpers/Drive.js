4439411cb6a3620ada6e7661da12d9e7524644b3
Cristian O. <84862634+Criser2013@users.noreply.github.com>
2026-06-24 12:10:32 -0500

    Funcionalidades de "DriveContextProvider" migradas en "DriveProvider".

--- src/helpers/Drive.js
+++ src/helpers/Drive.js
@@ -1,5 +1,5 @@
 import { buscarArchivo, crearArchivo, descargarArchivo, subirArchivo } from '../services/Drive';
-import { leerArchivoXlsx } from "../utils/XlsxFiles";
+import { crearArchivoXlsx, leerArchivoXlsx } from "../utils/XlsxFiles";
 import ArchivoPacientes from "../models/ArchivoPacientes";
 import { DRIVE_FILENAME, DRIVE_FOLDER_NAME } from "../../constants";
 
@@ -129,18 +129,17 @@ export default class DriveProvider {
      * - "success" (Boolean) - Indica si la operación fue exitosa o no.
      * - "error" (String) - Contiene el mensaje de error si la operación no fue exitosa, de lo contrario es null.
      */
-    #crearEstructuraArchivos() {
-        const resCarpeta = this.#crearArchivo(DRIVE_FOLDER_NAME, true);
+    async #crearEstructuraArchivos() {
+        const resCarpeta = await this.#crearArchivo(DRIVE_FOLDER_NAME, true);
         if (!resCarpeta.success) {
             return { success: false, error: resCarpeta.error };
         }
-        const resArchivo = this.#crearArchivo(DRIVE_FILENAME, false, resCarpeta.data.id);
+        const resArchivo = await this.#crearArchivo(DRIVE_FILENAME, false, resCarpeta.data.id);
         if (!resArchivo.success) {
             return { success: false, error: resArchivo.error };
         }
 
         this.#idArchivo = resArchivo.data.id;
-
         return { success: true };
     };
 
@@ -150,12 +149,12 @@ export default class DriveProvider {
      * - "success" (Boolean) - Indica si la operación fue exitosa o no.
      * - "error" (String) - Contiene el mensaje de error si la operación no fue exitosa, de lo contrario es null.
      */
-    #verificarEstructuraArchivos() {
-        const resCarpeta = this.#verificarExistenciaArchivo(DRIVE_FOLDER_NAME, true);
+    async #verificarEstructuraArchivos() {
+        const resCarpeta = await this.#verificarExistenciaArchivo(DRIVE_FOLDER_NAME, true);
         if (!resCarpeta.success) {
             return { success: false, error: resCarpeta.error };
         }
-        const resArchivo = this.#verificarExistenciaArchivo(DRIVE_FILENAME, false);
+        const resArchivo = await this.#verificarExistenciaArchivo(DRIVE_FILENAME, false);
         if (!resArchivo.success) {
             return { success: false, error: resArchivo.error };
         }
@@ -163,5 +162,169 @@ export default class DriveProvider {
         return { success: true };
     };
 
-    crearRespaldoDiagnosticos(token, nombreArchivo,)
+    /**
+     * Verifica si la estructura de archivos en Google Drive existe y, si no, la crea.
+     * Si existe, descarga el archivo de pacientes.
+     * @returns {Object} Resultado de la operación con las claves:
+     * - "success" (Boolean) - Indica si la operación fue exitosa o no.
+     * - "error" (String) - Contiene el mensaje de error si la operación no fue exitosa, de lo contrario es null.
+     */
+    async #actualizarEstado() {
+        const { success } = await this.#verificarEstructuraArchivos();
+
+        if (!success) {
+            const res = await this.#crearEstructuraArchivos();
+            if (!res.success) {
+                return { success: false, error: res.error };
+            }
+        } else {
+            const res = await this.#descargarArchivo(this.#idArchivo);
+            if (!res.success) {
+                return { success: false, error: res.error };
+            }
+        }
+        return { success: true };
+    };
+
+    /**
+     * Actualiza el archivo de pacientes en Google Drive con los datos actuales del archivo.
+     * @returns {Object} Resultado de la operación con las claves:
+     * - "success" (Boolean) - Indica si la operación fue exitosa o no.
+     * - "error" (String) - Contiene el mensaje de error si la operación no fue exitosa, de lo contrario es null.
+     */
+    async actualizarArchivo() {
+        const contCrudo = this.#archivo.toJson();
+        const contenido = crearArchivoXlsx(contCrudo, "Datos");
+
+        const { success, data, error } = await this.#subirArchivo(this.#idArchivo, contenido.data);
+
+        if (!success) {
+            return { success: false, error: error };
+        } else {
+            return { success: true };
+        }
+    };
+
+    /**
+     * @param {Paciente} paciente Instancia de paciente a añadir.
+     * @returns {Object} Resultado de la operación con las claves:
+     * - "success" (Boolean) - Indica si la operación fue exitosa o no.
+     * - "error" (String) - Contiene el mensaje de error si la operación no fue exitosa, de lo contrario es null.
+     */
+    async anadirPaciente(paciente) {
+        try {
+            const resActualizacion = await this.#actualizarEstado();
+            if (!resActualizacion.success) {
+                return { success: false, error: resActualizacion.error };
+            }
+            this.#archivo.anadirPaciente(paciente);
+            return await this.actualizarArchivo();
+        } catch {
+            return { success: false, error: "errPacienteDuplicado" };
+        }
+    };
+
+    /**
+     * Elimina pacientes del archivo de pacientes en Google Drive.
+     * @param {Array} idPacientes Array con los IDs de los pacientes a eliminar.
+     * @param {Boolean} varios Indica si se van a eliminar varios pacientes.
+     * @returns {Object} Resultado de la operación con las claves:
+     * - "success" (Boolean) - Indica si la operación fue exitosa o no.
+     * - "error" (String) - Contiene el mensaje de error si la operación no fue exitosa, de lo contrario es null.
+     */
+    async eliminarPacientes(idPacientes, varios = false) {
+        try {
+            const resActualizacion = await this.#actualizarEstado();
+            if (!resActualizacion.success) {
+                return { success: false, error: resActualizacion.error };
+            }
+
+            this.#archivo.eliminarPacientes(idPacientes, varios);
+            return await this.actualizarArchivo();
+        } catch {
+            return { success: false, error: "errPacienteInexistente" };
+        }
+    };
+
+    async modificarPaciente(id, paciente) {
+        try {
+            const resActualizacion = await this.#actualizarEstado();
+            if (!resActualizacion.success) {
+                return { success: false, error: resActualizacion.error };
+            }
+
+            this.#archivo.modificarPaciente(id, paciente);
+            return await this.actualizarArchivo();
+
+        } catch (error) {
+            return error.message.includes("ya existe") ?
+                { success: false, error: "errPacienteDuplicado" } :
+                { success: false, error: "errPacienteInexistente" };
+        }
+    };
+
+    async verPaciente(id) {
+        try {
+            const resActualizacion = await this.#actualizarEstado();
+            if (!resActualizacion.success) {
+                return { success: false, error: resActualizacion.error };
+            }
+            return await this.#archivo.verPaciente(id);
+        } catch {
+            return { success: false, error: "errPacienteInexistente" };
+        }
+    };
+
+    /**
+     * Crear una copia de los diagnósticos de la aplicación en Google Drive
+     * @param {String} nombreArchivo Nombre del archivo a crear en Google Drive.
+     * @param {Array<Object>} datos Arreglo de objetos con los datos a guardar en el archivo.
+     * @param {String} tipo Tipo de archivo a crear ("xlsx" o "csv").
+     * @returns {Object} Resultado de la operación con las claves:
+     * - "success" (Boolean) - Indica si la operación fue exitosa o no.
+     * - "error" (String) - Contiene el mensaje de error si la operación no fue exitosa, de lo contrario es null.
+     */
+    async crearCopiaDiagnosticos(nombreArchivo, datos, tipo = "xlsx") {
+        let idCarpeta = null;
+        const existe = await this.#verificarExisteArchivo(DRIVE_FOLDER_NAME, true);
+
+        if (!existe.success) {
+            const resCarpeta = await this.#crearArchivo(DRIVE_FOLDER_NAME, true);
+
+            if (!resCarpeta.success) {
+                return { success: false, error: resCarpeta.error };
+            }
+
+            idCarpeta = resCarpeta.data.id;
+        } else {
+            idCarpeta = existe.data.files[0].id;
+        }
+
+        return await this.#guardarArchivoDiagnosticos(nombreArchivo, idCarpeta, datos, tipo);
+    };
+
+    /**
+     * Guardar los diagnósticos de la aplicación en Google Drive
+     * @param {String} nombreArchivo Nombre del archivo a crear en Google Drive.
+     * @param {String} idCarpeta ID de la carpeta donde se guardará el archivo.
+     * @param {Array<Object>} datos Arreglo de objetos con los datos a guardar en el archivo.
+     * @param {String} tipo Tipo de archivo a crear ("xlsx" o "csv").
+     * @returns {Object} Resultado de la operación con las claves:
+     * - "success" (Boolean) - Indica si la operación fue exitosa o no.
+     * - "error" (String) - Contiene el mensaje de error si la operación no fue exitosa, de lo contrario es null.
+     */
+    async #guardarArchivoDiagnosticos(nombreArchivo, idCarpeta, datos, tipo) {
+        let mimeType = (tipo == "csv") ? "text/csv" : "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+        let res = await this.#crearArchivo(nombreArchivo, false, idCarpeta, mimeType);
+
+        if (!res.success) {
+            return { success: false, error: res.error };
+        }
+
+        const binario = crearArchivoXlsx(datos, tipo);
+        res = await this.#subirArchivo(res.data.id, binario.data, mimeType);
+
+        return res;
+    };
 };
