005f913bf05c31ff034341a843b69c50cc9d92af
Cristian O. <84862634+Criser2013@users.noreply.github.com>
2026-06-30 16:39:47 +0000

    Error de importación solucionado.

--- src/helpers/auth-helper.js
+++ src/helpers/auth-helper.js
@@ -1,4 +1,4 @@
-import { iniciarSesionGoogle, cerrarSesion, registrarUsuario, verRolUsuario, guardarCredsOAuth } from "../helpers/auth-helper";
+import { iniciarSesionGoogle, cerrarSesion, registrarUsuario, verRolUsuario, guardarCredsOAuth } from "../services/Autenticacion";
 
 /**
  * Inicia sesión con Google dentro de Firebase, registra al usuario en la base de datos si es su primera vez y obtiene su rol.
